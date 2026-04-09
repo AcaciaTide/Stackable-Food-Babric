614d357b9daddb39d7596700cb2dabb2b6c2f666
AcaciaTide <252184054+AcaciaTide@users.noreply.github.com>
2026-04-09 20:54:33 +0900

    fix: エラーを修正

--- src/main/java/io/github/acaciatide/stackablefoodstapi/config/FoodStackConfig.java
+++ src/main/java/io/github/acaciatide/stackablefoodstapi/config/FoodStackConfig.java
@@ -3,6 +3,6 @@ package io.github.acaciatide.stackablefoodstapi.config;
 import net.glasslauncher.mods.gcapi3.api.ConfigEntry;
 
 public class FoodStackConfig {
-    @ConfigEntry(name = "Max Food Stack Size", maxLength = 64)
+    @ConfigEntry(name = "Max Food Stack Size", maxValue = 64.0)
     public Integer maxFoodStackSize = 64;
 }
