1290f1f7d1b3cdb21c48ef7e0696f001aa85c091
AcaciaTide <252184054+AcaciaTide@users.noreply.github.com>
2026-06-26 05:45:13 +0900

    fix: マルチプレイで同期されるように修正

--- src/main/java/io/github/acaciatide/stackablefoodbabric/config/FoodStackConfig.java
+++ src/main/java/io/github/acaciatide/stackablefoodbabric/config/FoodStackConfig.java
@@ -3,6 +3,10 @@ package io.github.acaciatide.stackablefoodbabric.config;
 import net.glasslauncher.mods.gcapi3.api.ConfigEntry;
 
 public class FoodStackConfig {
-    @ConfigEntry(name = "Max Food Stack Size", maxValue = 64.0, requiresRestart = true)
+    @ConfigEntry(name = "Max Food Stack Size",
+            maxValue = 64.0,
+            requiresRestart = true,
+            multiplayerSynced = true
+    )
     public Integer maxFoodStackSize = 64;
 }
